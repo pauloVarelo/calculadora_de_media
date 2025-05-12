92e69a436e048084d64a00cb09a476eb851e2e1f
Paulo Henrique <61802190+pauloVarelo@users.noreply.github.com>
2025-05-12 16:45:34 -0300

    Feat: Calcular media

---
+++ app.py
@@ -0,0 +1,12 @@
+nome = input("Qual é seu nome? ")
+idade = int(input("Qual é sua idade? "))
+nacionalidade = input("Qual é sua Nacionalidade? ")
+cpf = input("Digite seu cpf? ")
+
+print("--------------------------")
+
+print(f"Seu nome é: {nome}")
+print(f"Sua idade é: {idade}")
+print(f"Sua nacionalidade é: {nacionalidade}")
+print(f"Seu cpf é: {cpf}")
+
